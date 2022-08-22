528d895a9847e54c3360de2cf440429ca1e1dfdf
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-22 18:55:40 -0300

    att-aulas

---
+++ fundamentos/tipoNumber.js
@@ -0,0 +1,18 @@
+// tipo number 
+
+const meuNumero = 3; 
+
+const primeiroNumero = 1;
+const segundoNumero = 2 ;
+const operacaoMatematica = primeiroNumero + segundoNumero; 
+
+console.log(operacaoMatematica);
+
+const numeroFlutuante = 3.3;
+const numeroFlutuante2 = .5; 
+const novaOperacao = primeiroNumero / numeroFlutuante; 
+console.log(novaOperacao);
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
---
+++ fundamentos/tipoString.js
@@ -0,0 +1,21 @@
+const texto1 = "Olá Mundo!";
+const texto2 = ' Eu sou Pedro!'
+
+
+// concatenação de texto
+const meuNome = texto1 + texto2;
+//console.log(meuNome);
+
+const tolower = texto2.toLocaleLowerCase(); 
+
+const up = texto2.toUpperCase(); 
+
+//console.log(tolower);
+//console.log(up);
+
+//booleans 
+
+const primeiroNumero = 5; 
+const segundoNumero = 10;
+
+console.log(primeiroNumero != segundoNumero);
