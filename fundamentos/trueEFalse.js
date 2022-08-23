2c4732a7c3872acff21cdd2021ec935d2ee8e5a9
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-23 10:06:55 -0300

    att-aulas

---
+++ fundamentos/trueEFalse.js
@@ -0,0 +1,28 @@
+// boolean 
+
+const usuariaLogado = true; 
+const contaPaga = false; 
+
+// truthy ou falsy 
+
+// 0 => false 
+// 1 => true 
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null 
+// undefined
+
+let minhaVar; 
+let varNull = null; 
+
+console.log(minhaVar);
+console.log(varNull);
+ let numero = 3;
+ let texto = "Alura"
+
+ console.log(typeof minhaVar);
+ console.log(typeof varNull);
+
--- fundamentos/varLetConst.js
+++ fundamentos/varLetConst.js
@@ -20,6 +20,20 @@ if(forma === 'retângulo') {
     area = (altura2 * comprimento2) / 2;
 }   
 
+//console.log(area);
 
+// const
 
-console.log(area);
+const forma2 = "quadrado"; 
+const altura3 = 5; 
+const comprimento3 = 5; 
+let area2; 
+
+if(forma2 === "quadrado"){
+    area2 = altura3 * comprimento3; 
+} else {
+    area2 = (altura3 * comprimento3) / 2;
+}
+
+console.log(area2);
+ 
