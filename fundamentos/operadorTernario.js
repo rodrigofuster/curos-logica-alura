9955f1e2592def09e1a1f7b3d2f0f6e632946f3d
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-24 09:53:38 -0300

    Create operadorTernario.js

---
+++ fundamentos/operadorTernario.js
@@ -0,0 +1,13 @@
+// operador ternário 
+
+const idadeMinima = 18; 
+const idadeCliente = 18; 
+
+/*
+if (idadeMinima > idadeCliente) {
+    console.log("Não pode beber!");
+} else {
+    console.log("Pode beber!");
+} */ 
+
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
