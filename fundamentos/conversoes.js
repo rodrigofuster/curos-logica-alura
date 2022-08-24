ae4e4ed794712f17cab82a0fbaf9084d226db6e0
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-08-24 08:39:54 -0300

    Create conversoes.js

---
+++ fundamentos/conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado 
+// booleanos 
+
+// conversão implícita
+ const numero = 456; 
+ const numeroString = "456"
+
+ //console.log(numero == numeroString);
+
+
+// convenção explícita 
+
+console.log(numero + Number(numeroString));
+
